a57da78d04525d2034689f5173c178817b210cf1
Mangesh2042 <mangesh2042@gmail.com>
2022-03-20 09:33:03 +0530

    grocey list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Grocery from './Grocery';
+ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Grocery />
   </React.StrictMode>,
   document.getElementById('root')
 );
